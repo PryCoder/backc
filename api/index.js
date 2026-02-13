66c9f4fd55e71be90ad93f9386caf359e17d6163
Priyanshu Gupta <priyanshugupta007007@gmail.com>
2026-02-14 00:25:10 +0530

    solved cors maintainability

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(helmet({
 }));
 app.use(compression());
 app.use(cors({
-  origin:  "https://avx-delta.vercel.app" || "",
+  origin:  "https://avx-delta.vercel.app" ,
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
